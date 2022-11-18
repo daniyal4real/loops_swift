31da7f6c0c07e34406ce5dd47e0a27c443f60504
Daniyal Ganiuly <daniyal.ganiuly@gmail.com>
2022-11-18 12:53:24 +0600

    initial commit

---
+++ lesson_itstep/main.swift
@@ -0,0 +1,94 @@
+//
+//  main.swift
+//  lesson_itstep
+//
+//  Created by Daniyal Ganiuly on 18.11.2022.
+//
+
+import Foundation
+import SwiftUI
+
+
+
+//var counter = 0
+//var max_num = 10
+//while counter <= max_num {
+//    print(counter)
+//    counter += 1
+//}
+
+
+
+//var count = 1
+//var age = 22
+//
+//while count <= age {
+//    if age % 2 == 0 && count % 2 == 0 {
+//        print(count)
+//    } else if age % 2 != 0 && count % 2 != 0 {
+//        print(count)
+//    }
+//    count += 1
+//}
+
+
+//var sum = 0
+//for i in 5...55 where i % 2 != 0 {
+//    sum = sum + i
+//}
+//print(sum)
+
+
+//var product = 1
+//
+//for i in 1...10 {
+//    product = product * i
+//}
+//print(product)
+
+
+
+//var product2 = 1
+//var min = 5
+//var max = 13
+//while min <= max {
+//    product2 *= min
+//    min += 1
+//}
+//print(product2)
+
+
+//let treeHeight = 5
+//let treeWidth = treeHeight * 2 - 1
+//
+//for lineNumber in 1...treeHeight {
+//
+//    // How many asterisks to print
+//    let stars = 2 * lineNumber - 1
+//    var line = ""
+//
+//    // Half the non-star space
+//    let spaces = (treeWidth - stars) / 2
+//    if spaces > 0 {
+//        line = String(repeating: " ", count: spaces)
+//    }
+//
+//    line += String(repeating: "*", count: stars)
+//    print (line)
+//}
+
+
+//var num = 10
+//
+//for i in 1..<num{
+//   for _ in 1...(num-i){
+//      print(terminator: " ")
+//   }
+//   for _ in 1...i{
+//       if i % 2 == 0 {
+//           continue
+//       }
+//      print("*", terminator: " ")
+//   }
+//   print("")
+//}
